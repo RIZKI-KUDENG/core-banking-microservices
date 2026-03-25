47b2eb9b0bc0ef271a15256003d3fb59eccf7387
M Rizki Arya Nanda <rizkiarya0611@gmail.com>
2026-03-25 20:43:50 +0700

    add dbContext

---
+++ services/transaction-service/src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<AppDbContext>(options => 
+        options.UseNpgsql(connectionString)
+        );
+
+        return services;
+
+    }
+}
